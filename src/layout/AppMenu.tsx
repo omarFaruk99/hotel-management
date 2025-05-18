fa9b05c538632542f66a08efffdb8d8b44ee321b
omarFaruk <2035faruk@gmail.com>
2025-05-18 18:10:34 +0600

    add 'label-table in APPMenu

--- src/layout/AppMenu.tsx
+++ src/layout/AppMenu.tsx
@@ -1,22 +1,27 @@
-import type {MenuModel} from "../../types";
+import type { MenuModel } from "../../types";
 import AppSubMenu from "./AppSubMenu";
 
 const AppMenu = () => {
-    const model: MenuModel[] = [
+  const model: MenuModel[] = [
+    {
+      label: "Dashboards",
+      icon: "pi pi-home",
+      items: [
         {
-            label: "Dashboards",
-            icon: "pi pi-home",
-            items: [
-                {
-                    label: "Home",
-                    icon: "pi pi-fw pi-home",
-                    to: "/",
-                },
-            ],
+          label: "Home",
+          icon: "pi pi-fw pi-home",
+          to: "/",
         },
-    ];
+        {
+          label: "Table",
+          icon: "pi pi-fw pi-table",
+          to: "/table",
+        },
+      ],
+    },
+  ];
 
-    return <AppSubMenu model={model}/>;
+  return <AppSubMenu model={model} />;
 };
 
 export default AppMenu;
